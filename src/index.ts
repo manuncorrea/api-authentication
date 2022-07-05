bc100cc361ea1520b544a37c2dc8dbddd6b703a6
Emanuele Correa <manuncorrea@hotmail.com>
2022-07-05 11:06:04 -0300

    add o cors na api

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 import './database/connect';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
